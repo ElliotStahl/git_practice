14a4e2735736df8fc99849b07662f69c174592bb
Elliot <elliotj.stahl@gmail.com>
2023-07-27 10:55:22 -0400

    established chain of equations, but currently nonfunctional

--- Practice1/Kinematics_projectileOverCliff.js
+++ Practice1/Kinematics_projectileOverCliff.js
@@ -1,7 +1,12 @@
-function xWithA(h,v){
-	const t = timeTillImpact(h,-9.81)
-	const newPosition =(v*t)
-	return newPosition
+function xWithA(h,v0,degree){
+	const vx = v0*Math.cos(degree);
+	const vy = v0*Math.sin(degree);
+	const tApex = apexTime(vy);
+	const yMax = (tApex,vy)+h;
+	const tFall = timeTillImpact(yMax,-9.81);
+	const tTotal = tApex+tFall;
+	const distance = vx*tTotal
+	return distance
 }
 
 function timeTillImpact(y,a){
@@ -20,6 +25,20 @@ function yVelocity(v,degree){
 	let radians = degree*Math.PI/180
 	const velocity = v*Math.sin(45*Math.PI/180)
 	return velocity
+}
+
+function apexTime(v0){
+	const apexTime = v0/9.81
+	return apexTime
+}
+function apexHeight(t,v0){
+	const t = apexTime(v0)
+	const apexHeight = (.5*(-9.81)*t*t)+(v0*t)
+	return apexHeight
+}
+
+function totalTime(){
+	const totalTime = apexTiem
 }
 
 
