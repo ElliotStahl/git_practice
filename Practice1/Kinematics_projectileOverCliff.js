e477bceb59e6854342d5c5d21553768afd0698e9
Elliot <elliotj.stahl@gmail.com>
2023-07-27 10:00:21 -0400

    finished x and y velocity functions

--- Practice1/Kinematics_projectileOverCliff.js
+++ Practice1/Kinematics_projectileOverCliff.js
@@ -3,8 +3,24 @@ function xWithA(h,v){
 	const newPosition =(v*t)
 	return newPosition
 }
+
 function timeTillImpact(y,a){
 	const time = Math.sqrt((Math.abs(((2*y)/a))))
 	return time
 }
-console.log(xWithA(10,100))
+
+
+function xVelocity(v,degree){
+	let radians = degree*Math.PI/180
+	const velocity = v*Math.cos(45*Math.PI/180)
+	return velocity
+}
+
+function yVelocity(v,degree){
+	let radians = degree*Math.PI/180
+	const velocity = v*Math.sin(45*Math.PI/180)
+	return velocity
+}
+
+
+
